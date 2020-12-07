684fc63ed41daa8f9070d60ec376d03f04d40f03
Tousif Khan <tkhan7916@gmail.com>
2020-12-06 23:39:58 -0500

    first commit

---
+++ stack/pipelines.py
@@ -0,0 +1,34 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+import pymongo
+
+from scrapy.utils.project import get_project_settings
+from scrapy.exceptions import DropItem
+import logging
+
+class MongoDBPipeline(object):
+
+    def __init__(self):
+        settings = get_project_settings()
+        connection = pymongo.MongoClient(
+            settings['MONGODB_SERVER'],
+            settings['MONGODB_PORT']
+        )
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['MONGODB_COLLECTION']]
+
+    def process_item(self, item):
+        valid = True
+        for data in item:
+            if not data:
+                valid = False
+                raise DropItem("Missing {0}!".format(data))
+        if valid:
+            self.collection.insert(dict(item))
+            logging.log(logging.INFO, "Question added to MongoDB database!")
+        return item
